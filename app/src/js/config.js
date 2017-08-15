7f6d38b12754c5d849d7ee016ad3d9590a4f287d
Boris Grinshpun <borisg@securitydam.com>
2017-08-15 16:37:34 +0300

    id

--- app/src/js/config.js
+++ app/src/js/config.js
@@ -1,6 +1,6 @@
 module.exports  = {
 	script       : 'https://connect.soundcloud.com/sdk/sdk-3.0.0.js',
-	clientId     : 'd652006c469530a4a7d6184b18e16c81',
+	clientId     : 'f8e3c97f77ba40c62629a35efc17282c',
 	pageLimit    : 6,
 	historyLimit : 5
 };
